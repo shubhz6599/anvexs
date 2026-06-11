43d0bef671e94a15f133d0743b6e9a19f607bc62
Shubham Karkhile <shubhz6599@gmail.com>
2026-06-11 12:16:31 +0530

    Ok

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,9 @@
 // ============================================
 export const environment = {
   production: false,
-  apiUrl: 'https://api.anvexs.com/api',
-  apiUrl1: 'https://api.anvexs.com',
+  apiUrl: 'http://localhost:5000/api',
+  apiUrl1: 'http://localhost:5000',
   aesKey: 'AnvexsAES256SecretKey2024Secure!',
   aesIv: 'AnvexsIV16Bytes!',
+  googleClientId:'99767736709-t6jh642moqu6ae6bacd92t8rmhjh2qf4.apps.googleusercontent.com'
 };
